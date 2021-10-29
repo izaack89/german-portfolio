d88e74689dbef85f50a09c864249582e5c4cc6dc
German Ramirez <mitsuominagi@gmail.com>
2021-10-28 19:14:36 -0500

    Edited the background color

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import {Link as LinkR} from 'react-router-dom'
 import {Link as LinkS} from 'react-scroll'
 
 export const Nav = styled.nav`
-    background:${({scrollNav}) => (scrollNav ? '#000':'#00000075')};
+    background:${({scrollNav}) => (scrollNav ? '#334242e0':'#00000075')};
     height:80px;
     margin-top:-80px;
     display:flex;
